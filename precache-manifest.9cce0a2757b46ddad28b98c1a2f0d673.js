4ceaf5fc35738cf102463de4f188c09ca00a61ae
Denis <denfedweb@mail.ru>
2020-04-29 22:30:00 +0300

    Updates

---
+++ precache-manifest.9cce0a2757b46ddad28b98c1a2f0d673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9513cc49627120ea741e4b13f49ebb9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d5b152f40b5be5ed788",
+    "url": "/static/css/main.5d43e8c2.chunk.css"
+  },
+  {
+    "revision": "0cf140ae280b21832da1",
+    "url": "/static/js/2.866597e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.866597e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5b152f40b5be5ed788",
+    "url": "/static/js/main.c06dbb75.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
