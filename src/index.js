5a6d661db8d46037549465e50ba71d683c9aeee3
denfedweb <denfedweb@mail.ru>
2020-05-30 20:12:49 +0300

    added sentry io

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.sass';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 
+Sentry.init({dsn: "https://b113e932830142b69215dd22ee35cb91@o400454.ingest.sentry.io/5258905"});
 
 ReactDOM.render(<App />,document.getElementById('root'));
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,6 @@
 import React, {Suspense, lazy} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Loader from "./components/Loader/Loader";
-// import Skills from "./pages/skills/skills";
-// import About from "./pages/about/about";
-// import Works from "./pages/works/works";
-// import Contact from "./pages/contact/contact";
-// import Editor from "./pages/editor/editor";
-
-// import Home from "./pages/home/home";
-// import Blog from "./pages/blog/blog";
-// <Route path="/" exact component={Home}/>
 
 const Skills = lazy(() => import('./pages/skills/skills'));
 const About = lazy(() => import('./pages/about/about'));
@@ -24,7 +15,6 @@ function Routes() {
                 <Route path="/skills" component={Skills} />
                 <Route path="/about" component={About} />
                 <Route path="/works" component={Works} />
-                {/*<Route path="/blog" component={Blog} />*/}
                 <Route path="/contact" component={Contact} />
                 <Route path="/editor" component={Editor}/>
                 <Redirect to="/about" />
